0185726039e56bb2b44fceaec5793b3f6e2c5627
andrewbay <daddnbay@gmail.com>
2021-05-24 15:55:45 -0700

    commit tree 019036912df588c4b39d1f9e1e2c1714a4c2a7f0

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+// const gifts = ['teddy bear', 'drone', 'doll'];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+//     return gifts;
+// }
+// wrapGifts(gifts);
+
+
+
+
+function writeCards(names, eventName) {
+    const array = [];
+    for (let i = 0; i < names.length; i++) {
+    array.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    debugger;
+    }
+    return array;
+}
+
+function countDown() {
+    let subtract = 10; 
+        while (subtract >= 0) {
+            console.log(subtract);
+        subtract--;
+    }
+}
